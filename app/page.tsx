5cac1e225115b00ed6cf1b98abc19a3547a1ca0c
Lawrence Macharia <macharialawrence2011@gmail.com>
2023-12-24 15:58:28 +0300

    added content within the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,41 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <main className="">
-      <h1>Dropbox Clone</h1>
+      <div>
+        <div className="p-10 flex flex-col bg-[#2B2929] dark:bg-slate-800 text-white space-y-5">
+          <h1 className="text-5xl font-bold">
+            Welcome to Dropbox. <br />
+            <br />
+            Storing everything for you and your business needs. All in one
+            place.
+          </h1>
+          <p className="pb-20">
+            Enhance your personal storage with Dropbox, offering a simple and
+            efficient way to upload, organize, and access files from anywhere.
+            Securely store important documents and media, and experience the
+            convinience of easy file management and sharing in one centralized
+            solution.
+          </p>
+          <Link href="/dashboard" className="flex bg-blue-500 p-5 w-fit">
+            Try it for free!
+            <ArrowRight className="ml-10" />
+          </Link>
+        </div>
+      </div>
+      <p className="text-center font-bold text-xl pt-5">Disclaimer</p>
+      <p className="text-center font-light p-2">
+        This app is made from a video meant for informational and educational
+        purposes only. We do not own or affiliate with Dropbox or/and any of its
+        subsidiaries in any form. Copyright Disclaimer under section 107 of the
+        Copyright Act 1976, allowance is made for "fair use" of this video for
+        education purposes only.
+      </p>
     </main>
-  )
+  );
 }
