ee39d722e16b161a2349dc57ca6d1266982eb74a
Lawrence Macharia <macharialawrence2011@gmail.com>
2023-12-24 15:58:53 +0300

    There's now a ThemeToggler

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
+import { ThemeToggler } from "./ThemeToggler";
 
 function Header() {
   return (
@@ -18,6 +19,7 @@ function Header() {
         <h1 className="font-bold text-xl">Dropbox</h1>
       </Link>
       <div className="px-5 flex space-x-2 items-center">
+        <ThemeToggler/>
         <UserButton afterSignOutUrl="/" />
         <SignedOut>
           <SignInButton afterSignInUrl="/dashboard" mode="modal" />
