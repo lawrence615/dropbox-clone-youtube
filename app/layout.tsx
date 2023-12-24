8de687064cc0860d2e2bd6a3a9ec8b44545efac1
Lawrence Macharia <macharialawrence2011@gmail.com>
2023-12-24 15:58:06 +0300

    Wrapped the Header and children within the ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
 import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,8 +21,15 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <Header />
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
