86d828c1da264e6b03c381939d7d5da86ae9d2bf
Lawrence Macharia <macharialawrence2011@gmail.com>
2023-12-24 17:03:33 +0300

    Added a video on the right which drops down on small screens.

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <main className="">
-      <div>
+      <div className="flex flex-col lg:flex-row items-center bg-[#1E1919] dark:bg-slate-800 ">
         <div className="p-10 flex flex-col bg-[#2B2929] dark:bg-slate-800 text-white space-y-5">
           <h1 className="text-5xl font-bold">
             Welcome to Dropbox. <br />
@@ -26,6 +26,12 @@ export default function Home() {
             Try it for free!
             <ArrowRight className="ml-10" />
           </Link>
+        </div>
+        <div className="bg-[#1E1919] dark:bg-slate-800 h-full p-10">
+          <video autoPlay loop muted className="rounded-lg">
+            <source src="https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/overview/lp-header-graphite200-1920x1080.mp4" />
+            Your browser does not support the video tag
+          </video>
         </div>
       </div>
       <p className="text-center font-bold text-xl pt-5">Disclaimer</p>
