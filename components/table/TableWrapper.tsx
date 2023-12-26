67e7002b3a1f7caec46cb348eb144c1c6d0eab85
Lawrence Macharia <macharialawrence2011@gmail.com>
2023-12-26 20:04:36 +0300

    initial commit - it will be used to build out the table

---
+++ components/table/TableWrapper.tsx
@@ -0,0 +1,7 @@
+import { FileType } from "@/typings";
+
+function TableWrapper({ skeletonFiles }: { skeletonFiles: FileType[] }) {
+  return <div>TableWrapper</div>;
+}
+
+export default TableWrapper;
