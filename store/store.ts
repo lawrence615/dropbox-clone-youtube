dabdb8ed90242b7ddbcd976bcb6218624e8bd5a9
Lawrence Macharia <macharialawrence2011@gmail.com>
2023-12-29 08:02:27 +0300

    added useAppState based on the interface which will be globally accessible through the app

--- store/store.ts
+++ store/store.ts
@@ -13,3 +13,17 @@ interface AppState {
   filename: string;
   setFilename: (filename: string) => void;
 }
+
+export const useAppState = create<AppState>()((set) => ({
+  fileId: null,
+  setFileId: (fileId: string) => set((state) => ({ fileId })),
+
+  filename: "",
+  setFilename: (filename: string) => set((state) => ({ filename })),
+
+  isDeleteModalOpen: false,
+  setIsDeleteModalOpen: (open) => set((state) => ({ isDeleteModalOpen: open })),
+
+  isRenameModalOpen: false,
+  setIsRenameModalOpen: (open) => set((state) => ({ isRenameModalOpen: open })),
+}));
