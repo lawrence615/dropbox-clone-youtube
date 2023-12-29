d8c6e0e1573c05c56abd234ed24defb7c58e1b85
Lawrence Macharia <macharialawrence2011@gmail.com>
2023-12-29 07:38:28 +0300

    an interface that resembles state of the app

---
+++ store/store.ts
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+
+interface AppState {
+  isDeleteModalOpen: boolean;
+  setIsDeleteModalOpen: (open: boolean) => void;
+
+  isRenameModalOpen: boolean;
+  setIsRenameModalOpen: (open: boolean) => void;
+
+  fileId: string | null;
+  setFileId: (fileId: string) => void;
+
+  filename: string;
+  setFilename: (filename: string) => void;
+}
