e7551d4582607e19053cce24efc1bd67f50fc4f4
Lawrence Macharia <macharialawrence2011@gmail.com>
2023-12-29 07:27:43 +0300

    added a nested a nested ternary operator to show how cell modification can be done outside the columns file

--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -17,7 +17,7 @@ import {
 } from "@/components/ui/table";
 import { FileType } from "@/typings";
 import { Button } from "../ui/button";
-import { TrashIcon } from "lucide-react";
+import { PencilIcon, TrashIcon } from "lucide-react";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -33,6 +33,17 @@ export function DataTable<TData, TValue>({
     columns,
     getCoreRowModel: getCoreRowModel(),
   });
+
+  const openDeleteModal = (fieldId: string) => {
+    setField(fieldId);
+    setIsDeleteModalOpen(true);
+  };
+
+  const openRenameModal = (fieldId: string, filename: string) => {
+    setField(fieldId);
+    setFilename(filename);
+    setIsRenameModalOpen(true);
+  };
 
   return (
     <div className="rounded-md border">
@@ -64,14 +75,36 @@ export function DataTable<TData, TValue>({
               >
                 {row.getVisibleCells().map((cell) => (
                   <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    {cell.column.id === "timestamp" ? (
+                      <div className="flex flex-col">
+                        <div className="text-sm">
+                          {(cell.getValue() as Date).toLocaleDateString()}
+                        </div>
+                        <div className="text-xs text-gray-500">
+                          {(cell.getValue() as Date).toLocaleTimeString()}
+                        </div>
+                      </div>
+                    ) : cell.column.id === "filename" ? (
+                      <p
+                        onClick={() => {
+                          console.log("Edit");
+                          // openRenameModal((row.original as FileType).id,(row.original as FileType).filename)
+                        }}
+                        className="underline flex items-center text-blue-500 hover:cursor-pointer"
+                      >
+                        {cell.getValue() as string}{" "}
+                        <PencilIcon size={15} className="ml-2" />
+                      </p>
+                    ) : (
+                      flexRender(cell.column.columnDef.cell, cell.getContext())
+                    )}
                   </TableCell>
                 ))}
                 <TableCell key={(row.original as FileType).id}>
                   <Button
                     variant={"outline"}
                     onClick={() => {
-                      console.log("Delete:",(row.original as FileType).id);
+                      console.log("Delete:", (row.original as FileType).id);
                       // openDeleteModal((row.original as FileType).id)
                     }}
                   >
